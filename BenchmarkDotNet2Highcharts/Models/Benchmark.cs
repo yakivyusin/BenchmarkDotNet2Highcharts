e652c846d8ae14a30f81a0f6ce94e3f06f7d0919
yakivyusin <yusin.yakiv@gmail.com>
2019-11-03 13:36:38 +0200

    Added Benchmark Model Class

---
+++ BenchmarkDotNet2Highcharts/Models/Benchmark.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BenchmarkDotNet2Highcharts.Models
+{
+    internal class Benchmark
+    {
+        public string DisplayInfo { get; set; }
+        public string Namespace { get; set; }
+        public string Type { get; set; }
+        public string Method { get; set; }
+        public string Parameters { get; set; }
+        public BenchmarkStatistics Statistics { get; set; }
+
+        public Dictionary<string, string> ParameterValues => Parameters?.Split('&')
+            .Select(p => p.Split('='))
+            .ToDictionary(p => p[0], p => p[1]);
+    }
+}
