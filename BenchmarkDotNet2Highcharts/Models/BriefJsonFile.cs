751a9d6f6c797c0d3fefa43bfc75cd658c77177f
yakivyusin <yusin.yakiv@gmail.com>
2019-11-03 13:39:05 +0200

    Added Brief Json File Model Class

---
+++ BenchmarkDotNet2Highcharts/Models/BriefJsonFile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BenchmarkDotNet2Highcharts.Models
+{
+    internal class BriefJsonFile
+    {
+        public EnvironmentInfo HostEnvironmentInfo { get; set; }
+        public List<Benchmark> Benchmarks { get; set; }
+    }
+}
