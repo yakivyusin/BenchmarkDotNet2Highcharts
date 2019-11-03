6b847bf0f72e19b8b7c524ad3e5825dd3f9a1e7c
yakivyusin <yusin.yakiv@gmail.com>
2019-11-03 13:28:09 +0200

    Added Environment Info Model Class

---
+++ BenchmarkDotNet2Highcharts/Models/EnvironmentInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BenchmarkDotNet2Highcharts.Models
+{
+    internal class EnvironmentInfo
+    {
+        public string BenchmarkDotNetVersion { get; set; }
+        public string OsVersion { get; set; }
+        public string ProcessorName { get; set; }
+        public string RuntimeVersion { get; set; }
+    }
+}
