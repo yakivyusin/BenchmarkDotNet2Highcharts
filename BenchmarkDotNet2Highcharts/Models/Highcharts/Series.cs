c4fcd02698e118b77069fd25a5b894a17831a4e7
yakivyusin <yusin.yakiv@gmail.com>
2019-11-09 19:21:53 +0200

    Added Series Highcharts Model

---
+++ BenchmarkDotNet2Highcharts/Models/Highcharts/Series.cs
@@ -0,0 +1,9 @@
+﻿namespace BenchmarkDotNet2Highcharts.Models.Highcharts
+{
+    internal class Series
+    {
+        public string Name { get; set; }
+        public decimal[][] Data { get; set; }
+        public Tooltip Tooltip { get; set; }
+    }
+}
