9c76b6ad3e8e272f280a1ecf1be12fee5707a6cc
yakivyusin <yusin.yakiv@gmail.com>
2019-11-09 19:14:50 +0200

    Added XAxis Highcharts Model

---
+++ BenchmarkDotNet2Highcharts/Models/Highcharts/XAxis.cs
@@ -0,0 +1,8 @@
+﻿namespace BenchmarkDotNet2Highcharts.Models.Highcharts
+{
+    internal class XAxis
+    {
+        public string[] Categories { get; set; }
+        public Title Title { get; set; }
+    }
+}
