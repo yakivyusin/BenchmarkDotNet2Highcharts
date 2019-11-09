ea1cad6bf054e95c1b2cdc52d2a7da2918fad2a6
yakivyusin <yusin.yakiv@gmail.com>
2019-11-09 19:23:55 +0200

    Added Plot Highcharts Model

---
+++ BenchmarkDotNet2Highcharts/Models/Highcharts/Plot.cs
@@ -0,0 +1,12 @@
+﻿namespace BenchmarkDotNet2Highcharts.Models.Highcharts
+{
+    internal class Plot
+    {
+        public Chart Chart { get; set; }
+        public Title Title { get; set; }
+        public Legend Legend { get; set; }
+        public XAxis XAxis { get; set; }
+        public YAxis YAxis { get; set; }
+        public Series[] Series { get; set; }
+    }
+}
