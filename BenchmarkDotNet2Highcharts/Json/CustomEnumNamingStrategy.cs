6745628e26db077c9f32677cb77bb209a4854b52
yakivyusin <yusin.yakiv@gmail.com>
2019-11-15 19:09:01 +0200

    Added Custon Enum Naming Strategy for Serialization

---
+++ BenchmarkDotNet2Highcharts/Json/CustomEnumNamingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace BenchmarkDotNet2Highcharts.Json
+{
+    internal class CustomEnumNamingStrategy : CamelCaseNamingStrategy
+    {
+        public override string GetPropertyName(string name, bool hasSpecifiedName)
+        {
+            return base.GetPropertyName(name, hasSpecifiedName).ToLower();
+        }
+    }
+}
