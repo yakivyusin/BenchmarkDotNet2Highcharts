d2377018f5af26403d250c35c6f7e61f534699c4
yakivyusin <yusin.yakiv@gmail.com>
2019-11-15 19:30:57 +0200

    Added Serializing Settings Unit Tests

---
+++ BenchmarkDotNet2Highcharts.UnitTests/Helpers/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BenchmarkDotNet2Highcharts.UnitTests.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string RemoveWhitespaces(this string text)
+        {
+            return text
+                .Replace("\t", string.Empty)
+                .Replace(Environment.NewLine, string.Empty)
+                .Replace(" ", string.Empty);
+        }
+    }
+}
---
+++ BenchmarkDotNet2Highcharts.UnitTests/SerializingTests.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet2Highcharts.Json;
+using BenchmarkDotNet2Highcharts.UnitTests.Helpers;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace BenchmarkDotNet2Highcharts.UnitTests
+{
+    [TestFixture]
+    public class SerializingTests
+    {
+        private readonly SerializingSettings serializingSettings = new SerializingSettings();
+
+        [Test]
+        public void PropertyNamesAsCamelCase()
+        {
+            var obj = new
+            {
+                Property = 1,
+                PropertyTwo = 2
+            };
+
+            var json = JsonConvert.SerializeObject(obj, serializingSettings)
+                .RemoveWhitespaces();
+
+            Assert.AreEqual(@"{""property"":1,""propertyTwo"":2}", json);
+        }
+
+        [Test]
+        public void EnumValuesAsLowerCase()
+        {
+            var obj = new
+            {
+                A = LowerCaseEnum.Value,
+                B = LowerCaseEnum.ValueTwo,
+                C = LowerCaseEnum.VT
+            };
+
+            var json = JsonConvert.SerializeObject(obj, serializingSettings)
+                .RemoveWhitespaces();
+
+            Assert.AreEqual(@"{""a"":""value"",""b"":""valuetwo"",""c"":""vt""}", json);
+        }
+
+        private enum LowerCaseEnum
+        {
+            Value,
+            ValueTwo,
+            VT
+        }
+    }
+}
