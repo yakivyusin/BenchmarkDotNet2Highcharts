7ac9ab1624e4872baf2ac23a2d545589a332ef1a
yakivyusin <yusin.yakiv@gmail.com>
2019-11-15 19:39:42 +0200

    Added Plot Factory

---
+++ BenchmarkDotNet2Highcharts/Models/PlotFactory.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet2Highcharts.Models.Highcharts;
+
+namespace BenchmarkDotNet2Highcharts.Models
+{
+    internal class PlotFactory
+    {
+        public Plot CreateBoxPlot(string title)
+        {
+            return new Plot
+            {
+                Chart = new Chart
+                {
+                    Type = ChartType.BoxPlot,
+                    ZoomType = ZoomType.XY
+                },
+                Title = new Title
+                {
+                    Text = title
+                },
+                Legend = new Legend(),
+                XAxis = new XAxis(),
+                YAxis = new YAxis()
+            };
+        }
+    }
+}
