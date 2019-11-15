5ae4443c9c6fe29f4e1373dd9af43cddb7464da1
yakivyusin <yusin.yakiv@gmail.com>
2019-11-15 19:34:07 +0200

    Added Deserializing Settings Unit Tests

---
+++ BenchmarkDotNet2Highcharts.UnitTests/DeserializingTests.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet2Highcharts.Json;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace BenchmarkDotNet2Highcharts.UnitTests
+{
+    [TestFixture]
+    public class DeserializingTests
+    {
+        private readonly DeserializingSettings deserializingSettings = new DeserializingSettings();
+
+        [Test]
+        public void FloatingPointAsDecimal()
+        {
+            var json = @"{""Property"":3.1415}";
+
+            var obj = JsonConvert.DeserializeObject<DecimalClass>(json, deserializingSettings);
+
+            Assert.AreEqual(3.1415m, obj.Property);
+        }
+
+        private class DecimalClass
+        {
+            public decimal Property { get; set; }
+        }
+    }
+}
