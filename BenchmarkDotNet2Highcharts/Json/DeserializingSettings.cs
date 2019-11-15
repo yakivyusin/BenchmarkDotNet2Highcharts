6153ddda1311d7aae7ba34f1ca9a846eb4c8a157
yakivyusin <yusin.yakiv@gmail.com>
2019-11-15 18:47:42 +0200

    Added Custom Deserializing Settings

---
+++ BenchmarkDotNet2Highcharts/Json/DeserializingSettings.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BenchmarkDotNet2Highcharts.Json
+{
+    internal class DeserializingSettings : JsonSerializerSettings
+    {
+        public DeserializingSettings()
+        {
+            FloatParseHandling = FloatParseHandling.Decimal;
+            NullValueHandling = NullValueHandling.Ignore;
+        }
+    }
+}
