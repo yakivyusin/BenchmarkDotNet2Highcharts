09117295057ed765c0f89674baff23228d0907de
yakivyusin <yusin.yakiv@gmail.com>
2019-11-15 19:11:43 +0200

    Added Custom Json Serializing Settings

---
+++ BenchmarkDotNet2Highcharts/Json/SerializingSettings.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+
+namespace BenchmarkDotNet2Highcharts.Json
+{
+    internal class SerializingSettings : JsonSerializerSettings
+    {
+        public SerializingSettings()
+        {
+            Formatting = Formatting.Indented;
+            NullValueHandling = NullValueHandling.Ignore;
+            ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Converters = new List<JsonConverter>
+            {
+                new StringEnumConverter(new CustomEnumNamingStrategy(), false)
+            };
+        }
+    }
+}
