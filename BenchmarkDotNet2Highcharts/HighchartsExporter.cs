32a5cc5a5902efe944ce0e44e5b8771f56f347df
yakivyusin <yusin.yakiv@gmail.com>
2019-12-07 18:47:32 +0200

    Added Highcharts Exporter Class Stub

---
+++ BenchmarkDotNet2Highcharts/HighchartsExporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BenchmarkDotNet2Highcharts
+{
+    public class HighchartsExporter
+    {
+        private const string DefaultFolderPath = "./BenchmarkDotNet.Artifacts/results";
+
+        public HighchartsExporter() : this(DefaultFolderPath)
+        {
+
+        }
+
+        public HighchartsExporter(string folderPath)
+        {
+            if (folderPath == null)
+            {
+                throw new ArgumentNullException(nameof(folderPath));
+            }
+        }
+    }
+}
