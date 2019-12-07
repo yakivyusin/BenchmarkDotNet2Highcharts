18e0bfcc164713b1234f23aa641494d035d4d922
yakivyusin <yusin.yakiv@gmail.com>
2019-12-07 18:53:06 +0200

    Added Benchmark Loader

---
+++ BenchmarkDotNet2Highcharts/Business/BenchmarkLoader.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet2Highcharts.Models.BenchmarkDotNet;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BenchmarkDotNet2Highcharts.Business
+{
+    internal class BenchmarkLoader
+    {
+        private const string BenchmarkMask = "*-report-brief.json";
+        private string _folderPath;
+
+        public BenchmarkLoader(string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+
+        public List<BriefJsonFile> Load()
+        {
+            var files = Directory.GetFiles(_folderPath, BenchmarkMask, SearchOption.TopDirectoryOnly);
+
+            return files
+                .Select(x => LoadFile(x))
+                .Where(x => x != null)
+                .ToList();
+        }
+
+        private BriefJsonFile LoadFile(string file)
+        {
+            var fileContent = File.ReadAllText(file);
+
+            try
+            {
+                return JsonConvert.DeserializeObject<BriefJsonFile>(fileContent);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
