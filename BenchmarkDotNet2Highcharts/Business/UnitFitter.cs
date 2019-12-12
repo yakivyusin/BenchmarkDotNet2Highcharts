6c222a734f8f13717c3c190aba26341ad0ed0e49
yakivyusin <yusin.yakiv@gmail.com>
2019-12-12 19:30:25 +0200

    Added UnitFitter

---
+++ BenchmarkDotNet2Highcharts/Business/UnitFitter.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet2Highcharts.Models;
+using System.Linq;
+
+namespace BenchmarkDotNet2Highcharts.Business
+{
+    internal static class UnitFitter
+    {
+        public static Unit ApplyBestFit(decimal[][] values)
+        {
+            var minValue = values.Min(x => x.Min());
+            var currentUnit = Unit.Nanosecond;
+
+            while (minValue >= ConvertUnitToNanoseconds(currentUnit) * 1000 && currentUnit <= Unit.Second)
+            {
+                currentUnit++;
+            }
+
+            if (currentUnit != Unit.Nanosecond)
+            {
+                var divideCoeff = ConvertUnitToNanoseconds(currentUnit);
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    for (int j = 0; j < values[i].Length; j++)
+                    {
+                        values[i][j] /= divideCoeff;
+                    }
+                }
+            }
+
+            return currentUnit;
+        }
+
+        private static decimal ConvertUnitToNanoseconds(Unit unit)
+        {
+            decimal value = 1;
+
+            for (int i = 0; i < (int)unit; i++)
+            {
+                value *= 1000;
+            }
+
+            return value;
+        }
+    }
+}
