f9d19229108e1278824113046323eb7533125829
yakivyusin <yusin.yakiv@gmail.com>
2019-12-14 16:14:29 +0200

    Added Collection Extensions

---
+++ BenchmarkDotNet2Highcharts/Helpers/CollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BenchmarkDotNet2Highcharts.Helpers
+{
+    internal static class CollectionExtensions
+    {
+        public static void OnEach<T>(IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }
+    }
+}
