b66c3ac616fd79bf0892a45b60c03378e193d173
yakivyusin <yusin.yakiv@gmail.com>
2019-12-14 16:11:17 +0200

    Added Unit Enum Extension to Get Translation

---
+++ BenchmarkDotNet2Highcharts/Helpers/UnitExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet2Highcharts.Models;
+using BenchmarkDotNet2Highcharts.Resources;
+
+namespace BenchmarkDotNet2Highcharts.Helpers
+{
+    internal static class UnitExtensions
+    {
+        public static string GetTranslation(this Unit unit)
+        {
+            var unitName = unit.ToString("G");
+
+            return Resource.ResourceManager.GetString($"Unit_{unitName}");
+        }
+    }
+}
