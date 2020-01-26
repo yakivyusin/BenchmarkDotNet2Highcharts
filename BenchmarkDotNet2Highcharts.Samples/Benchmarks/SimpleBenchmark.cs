1f9f7759a5358083119770baa3b2ef316b046f95
yakivyusin <yusin.yakiv@gmail.com>
2020-01-26 18:54:02 +0200

    Added Simple Benchmark Sample

---
+++ BenchmarkDotNet2Highcharts.Samples/Benchmarks/SimpleBenchmark.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Threading;
+
+namespace BenchmarkDotNet2Highcharts.Samples.Benchmarks
+{
+    [JsonExporterAttribute.Brief]
+    public class SimpleBenchmark
+    {
+        [Benchmark]
+        public void A() => Thread.Sleep(1200);
+
+        [Benchmark]
+        public void B() => Thread.Sleep(1500);
+    }
+}
