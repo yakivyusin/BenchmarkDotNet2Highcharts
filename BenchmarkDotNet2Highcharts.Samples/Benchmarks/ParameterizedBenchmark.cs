02d02afcccff27ccbd5a2a354dba29e858078327
yakivyusin <yusin.yakiv@gmail.com>
2020-01-26 18:55:59 +0200

    Added Parameterized Benchmark Sample

---
+++ BenchmarkDotNet2Highcharts.Samples/Benchmarks/ParameterizedBenchmark.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Threading;
+
+namespace BenchmarkDotNet2Highcharts.Samples.Benchmarks
+{
+    [JsonExporterAttribute.Brief]
+    public class ParameterizedBenchmark
+    {
+        [Params(200, 300)]
+        public int A { get; set; }
+
+        [Params(20, 30, 40)]
+        public int B { get; set; }
+
+        [Benchmark]
+        public void C() => Thread.Sleep(A + B);
+
+        [Benchmark]
+        public void D() => Thread.Sleep(A - B);
+    }
+}
