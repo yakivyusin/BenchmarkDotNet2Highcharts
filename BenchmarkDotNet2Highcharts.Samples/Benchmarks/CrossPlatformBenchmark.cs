849e6c791d963fb08ef7543d955eb1954b67ad80
yakivyusin <yusin.yakiv@gmail.com>
2020-01-26 19:17:11 +0200

    Added Cross-Platform Benchmark Sample

---
+++ BenchmarkDotNet2Highcharts.Samples/Benchmarks/CrossPlatformBenchmark.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System.Threading;
+
+namespace BenchmarkDotNet2Highcharts.Samples.Benchmarks
+{
+    [JsonExporterAttribute.Brief]
+    [SimpleJob(RuntimeMoniker.NetCoreApp22)]
+    [SimpleJob(RuntimeMoniker.Net472)]
+    public class CrossPlatformBenchmark
+    {
+        [Params(20, 30)]
+        public int A { get; set; }
+
+        [Benchmark]
+        public void B() => Thread.Sleep(500 + A);
+
+        [Benchmark]
+        public void C() => Thread.Sleep(700 + A);
+    }
+}
