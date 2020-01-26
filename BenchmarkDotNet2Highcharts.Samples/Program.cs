592f69d41710c07165e803f3d7200e671b9f56a3
yakivyusin <yusin.yakiv@gmail.com>
2020-01-26 18:49:32 +0200

    Setup Samples Project

---
+++ BenchmarkDotNet2Highcharts.Samples/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace BenchmarkDotNet2Highcharts.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            var hc = new HighchartsExporter();
+            hc.Export();
+        }
+    }
+}
